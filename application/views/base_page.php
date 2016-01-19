dcb49c2d12007acfed7734fdd45a47ad50d16f2c
zjm <zjmmjzzjm@126.com>
2016-01-19 20:48:10 +0600

    upate

--- application/views/base_page.php
+++ application/views/base_page.php
@@ -1,45 +1,75 @@
-<!DOCTYPE html>
-<html lang="en-us">
-<head>
-	<meta name="viewport" content="width=device-width" />
-	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
-	<meta name="renderer" content="webkit" />
-	<meta name="description" content="This is a powerful Magnet Link search engine that helps you discovery the DHT network." />
-	<meta name="keywords" content="btmilk, magnet link, torrent, magnet search, torrent search engine" />
-	<title>欢迎使用奶茶磁力链接搜索引擎</title>
-	<script src="/frontend/scripts/jquery.min.js"></script>
-	<link href="/frontend/styles/bootstrap.min.css" rel="stylesheet" />
-	<link href="/frontend/styles/site.css" rel="stylesheet" />
-	<link href="/frontend/styles/buttons.min.css" rel="stylesheet" />
-	<script src="/frontend/scripts/site.js?1203"></script>
-</head>
-<body style="background: url(/frontend/xxxxImages/pattern.png) repeat fixed;">
-	<div>
-		<div "header">
-			<a href="/"></a>
-		</div>
-	</div>
-
-	<div class="content">
-		<div>
-			<div style="text-align: center; margin-top: 100px;">
-				<DIV style=" margin-left: -30px;">
-					<img src="/frontend/images/titlePg.png"> 
+﻿<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.0 Transitional//EN">
+<!-- saved from url=(0022)http://www.btmilk.com/ -->
+<HTML lang="zh-CN">
+<HEAD>
+	<META content="IE=5.0000" http-equiv="X-UA-Compatible">
+	<META http-equiv="Content-Type" content="text/html; charset=utf-8">
+	<META name="GENERATOR" content="MSHTML 11.00.9600.18163">
+	<META http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
+	<META name="renderer" content="webkit"> 
+	<META name="viewport" content="width=device-width,initial-scale=1"> 
+	<META name="keywords" content="磁力链接,磁力搜索,磁力搜索,BT资源,迅雷下载,btmilk"> 
+	<META name="description" content="btmilk是一个磁力链接及BT种子搜索引擎,支持最新及热门电影,电视剧、动漫、综艺视频在线云播，超多影视番号、书籍、音乐、游戏、软件资源下载,是专业的磁力种子搜索神器。支持小米路由一键下载。}"> 
+	<LINK href="frontend/images/favicon.png" rel="icon"> 
+	<LINK href="frontend/styles/bootstrap.min.css" rel="stylesheet"> 
+	<LINK href="frontend/styles/feiliuzhixia.css" rel="stylesheet"> 
+	<TITLE>btmilk-磁力搜索-种子搜索-迅雷下载</TITLE> 
+	<!--img标签横幅广告位--> 
+	<SCRIPT src="frontend/scripts/jquery.min.js" />
+	<SCRIPT src="frontend/scripts/bootstrap.min.js" />
+	<!-- <SCRIPT src="css/opensug.js" charset="gbk" /> -->
+</HEAD>
+<BODY>
+	<SCRIPT>
+		var str;
+		function aa(){
+		str=document.getElementById("input1").value;
+		return str;
+		}
+	</SCRIPT>
+	<NAV class="navbar navbar-default navbar-fixed-top">
+		<DIV class="container">
+			<DIV class="navbar-header">
+				<BUTTON class="navbar-toggle collapsed" type="button" data-toggle="collapse" data-target="#navbar">
+					<SPAN class="sr-only">磁力链接</SPAN>
+					<SPAN class="icon-bar"> </SPAN>
+					<SPAN class="icon-bar"> </SPAN>
+					<SPAN class="icon-bar"> </SPAN>
+				</BUTTON>
+				<A class="navbar-brand" href="http://www.btmilk.com/">btmilk</A>			 </DIV>
+			<DIV class="navbar-collapse collapse" id="navbar">
+				<UL class="nav navbar-nav">
+					<LI>
+						<A href="http://www.btmilk.com/tpl/template/help.html" target="_blank">下载帮助</A>
+					</LI>
+				</UL>
+				<UL class="nav navbar-nav navbar-right">
+					<LI>
+						<A href="http://www.btmilk.com" target="_blank">交流求片</A>
+					</LI>
+				</UL>
+			</DIV>
+		</DIV>
+	</NAV>
+	<DIV class="container">
+		<DIV class="jumbotron">
+			<DIV class="container">
+				<A href="http://www.btmilk.com/tpl/template/zs.php" target="_blank">
+					<SPAN style="color: rgb(229, 51, 51);">点击查看总资源数</SPAN>
+				</A>			 
+				<DIV class="logo">
+					<img src="images/lcssogo.png" href="http://www.btmilk.com/"> 
+					<FORM action="" id="search_form_h" method="GET">
+						<DIV class="input-group">
+							<INPUT name="keyword" id="keyword" class="form-control" autofocus="" required="" type="text" placeholder="搜索电影、剧集、动漫、番号、演员、软件..." value="" baidusug="1">
+							<SPAN class="input-group-btn">
+								<button class="btn btn-default" type="submit"> <span class="glyphicon glyphicon-search"> </span> 搜索</button>
+							</SPAN>												</DIV>
+					</FORM>
 				</DIV>
-				<DIV>
-					<H3><SPAN style="margin-left: -17px;color: rgb(0, 0, 0); font-size: 16px;">全国最全最好的资源搜索引擎网站</SPAN></H3>
-				</DIV>
-				<form action="/index.php/" method="get" id="search_form_h" style="height: 80px;">
-					<div>
-						<input type="text" name="keyword" id="keyword" class="search_box" style="height: 35px; width: 400px;" />
-						<input type="submit" value="搜索" class="search_button" style="height: 35px; width: 100px;" />
-					</div>
-				</form>
-			</div>
-		</div>
-	</div>
-	<script type="text/javascript">
+			</DIV>
+		</DIV>
+		<script type="text/javascript">
 		$("#keyword").focus();
 		$("#search_form_h").submit(function (e) {
 			e.preventDefault();
@@ -54,5 +84,47 @@
 		
 				});
 	</script>
-</body>
-</html>
+		<DIV class="container">
+			<P class="center"> <!--更改下面电影的时候，左侧和右侧要一致，不能只修改一侧，例如:煎饼侠.html">煎饼侠</a>-->
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/夏洛特烦恼">夏洛特烦恼</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/港囧">港囧</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/烈日灼心">烈日灼心</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/心迷宫">心迷宫</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/蚁人">蚁人</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/photoshop">photoshop</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/office">office</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/煎饼侠">煎饼侠</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/左耳">左耳</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/捉妖记">捉妖记</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/西游记之大圣归来">西游记之大圣归来</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/超能陆战队">超能陆战队</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/木星上行">木星上行</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/生死阻击">生死阻击</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/windows">windows</A> 
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/加勒比">加勒比</A>
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/东京热">东京热</A> 
+			<A class="btn btn-default hotwords" href="http://www.btmilk.com/index.php/search/一本道">一本道</A>
+			</P>
+		</DIV>
+		</DIV>
+		<NAV class="footer navbar-inverse">
+			<DIV class="container">
+				<DIV class="navbar-collapse collapse navbar-text">
+					<P>简单、快速、高效、稳定、影视、音乐、软件、BT、种子</P>
+					<P>
+						<A href="http://www.btmilk.com/tpl/template/dcma.html">btmilk声明</A> | <A 
+							href="http://www.btmilk.com/tpl/template/sitemap.xml" 
+							target="_blank">网站地图</A>
+					</P>
+				</DIV>
+				<DIV class="navbar-text navbar-right">
+					<P>(c)2015 btmilk.com&nbsp;&nbsp;|&nbsp;
+						<SCRIPT language="JavaScript" src="css/stat.js">
+						</SCRIPT>
+					</P>
+					<P>btmilk磁力搜索引擎，第一好用的磁力搜索引擎</P>
+				</DIV>
+			</DIV>
+		</NAV>
+</BODY>
+</HTML>
