f269a88eafea232dd1e7e1372c42d8b9a9aa94da
zjm <zjmmjzzjm@126.com>
2016-04-15 17:31:28 +0600

    update

--- application/views/base_page.php
+++ application/views/base_page.php
@@ -2,19 +2,19 @@
 <HTML lang="zh-CN">
 <HEAD>
 	<TITLE>btmilk-磁力搜索-种子搜索-迅雷下载</TITLE> 
-	<META content="IE=5.0000" http-equiv="X-UA-Compatible">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 	<META http-equiv="Content-Type" content="text/html; charset=utf-8">
 	<META name="GENERATOR" content="MSHTML 11.00.9600.18163">
 	<META http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"> 
 	<META name="renderer" content="webkit"> 
 	<META name="viewport" content="width=device-width,initial-scale=1"> 
-	<META name="keywords" content="磁力链接,磁力搜索,磁力搜索,BT资源,迅雷下载,btmilk"> 
+	<META name="keywords" content="磁力链接,磁力搜索,磁力搜索,BT资源,迅雷下载,最新电影，免费电影"> 
 	<META name="description" content="btmilk是一个磁力链接及BT种子搜索引擎,支持最新及热门电影,电视剧、动漫、综艺视频在线云播，超多影视番号、书籍、音乐、游戏、软件资源下载,是专业的磁力种子搜索神器。支持小米路由一键下载。}"> 
 	<LINK href="/frontend/images/favor.jpg" rel="icon"> 
-	<LINK href="/frontend/styles/bootstrap.min.css" rel="stylesheet"> 
-	<LINK href="/frontend/styles/feiliuzhixia.css" rel="stylesheet"> 
-	<SCRIPT src="/frontend/scripts/jquery.min.js" />
-	<SCRIPT src="/frontend/scripts/bootstrap.min.js" />
+<link href="http://cdn.bootcss.com/bootstrap/3.3.4/css/bootstrap.min.css" rel="stylesheet">
+<link href="/frontend/styles/feiliuzhixia.css" rel="stylesheet">
+<script src="http://cdn.bootcss.com/jquery/1.11.2/jquery.min.js"></script>
+<script src="http://cdn.bootcss.com/bootstrap/3.3.4/js/bootstrap.min.js"></script>
 	<link rel="stylesheet" href="http://bdimg.share.baidu.com/static/api/css/slide_share.css?v=9c50d088.css">
 	<script>
 	var _hmt = _hmt || [];
@@ -60,12 +60,12 @@
 			<DIV class="container">
 				<DIV class="logo">
 					<div style="padding-bottom:20px;">	
-					<img style="max-width:100%;" src="/frontend/images/logo.png" href="http://www.btmilk.com/">
+					<img style="max-width:100%;" src="/frontend/images/logo.png" href="http://www.btmilk.com/" alt="最新电影">
 					</div>	 
 					
 					<FORM  action="/index.php/search" id="search_form_h" method="GET">
 						<DIV class="input-group">
-							<INPUT name="keyword" id="keyword" class="form-control" autofocus="" required="" type="text" placeholder="搜索电影、剧集、动漫、番号、演员、软件..." value="" baidusug="1">
+							<INPUT name="keyword" id="keyword" class="form-control" autofocus="" required="" type="text" placeholder="搜索电影、剧集、动漫、番号、演员、软件..." value="" baiduSug="1">
 							<SPAN class="input-group-btn">
 								<button class="btn btn-default" type="submit"> <span class="glyphicon glyphicon-search"> </span> 搜索</button>
 							</SPAN>												</DIV>
@@ -100,7 +100,8 @@
 			</DIV>
 		</NAV>
 	<script src="http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion=403685"></script>
-	<script src="../../frontend/scripts/opensug.js" charset="gbk"></script><iframe class="bdSug_sd" style="display: none; position: absolute; border-width: 0px;"></iframe><div id="bdSug_1453271036829" class="bdSug_wpr" style="display: none;"></div>
+	<script src="http://www.baidu.com/js/opensug.js" charset="gbk"></script>
+	<iframe class="bdSug_sd" style="display: none; position: absolute; border-width: 0px;"></iframe><div id="bdSug_1453271036829" class="bdSug_wpr" style="display: none;"></div>
 	<script>window._bd_share_config={"common":{"bdSnsKey":{},"bdText":"","bdMini":"2","bdMiniList":false,"bdPic":"","bdStyle":"0","bdSize":"16"},"slide":{"type":"slide","bdImg":"5","bdPos":"right","bdTop":"139.5"},"selectShare":{"bdContainerClass":null,"bdSelectMiniList":["qzone","tsina","tqq","renren","weixin"]}};with(document)0[(getElementsByTagName('head')[0]||body).appendChild(createElement('script')).src='http://bdimg.share.baidu.com/static/api/js/share.js?v=89860593.js?cdnversion='+~(-new Date()/36e5)];</script>
 
 </BODY>
